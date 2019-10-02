ec08ff08aa00d15bb36f21c7689b712ec7be0c52
Carlos Eduardo Souza de Paula <carlos.eduardo@mandic.net.br>
2019-10-02 17:08:13 -0300

    Chore: change default project kms name

--- functions/encrypt_secret/encrypt_secret.py
+++ functions/encrypt_secret/encrypt_secret.py
@@ -6,7 +6,7 @@ def encrypt(event, context):
     secret = event['secret']
     kms = boto3.client('kms')
     encrypted = kms.encrypt(
-        KeyId='alias/{}Protection'.format(os.environ.get('APPLICATION_NAME')),
+        KeyId='alias/{}Key'.format(os.environ.get('APPLICATION_NAME')),
         Plaintext=bytes(secret, 'utf-8')
     )
     return base64.b64encode(encrypted['CiphertextBlob'])
